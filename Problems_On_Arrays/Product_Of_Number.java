94807d19c7b74fc453c38144d79425b90ac6b758
OfficialDexter7 <dexyyy777@gmail.com>
2026-03-20 12:42:37 +0000

    comment

--- Problems_On_Arrays/Product_Of_Number.java
+++ Problems_On_Arrays/Product_Of_Number.java
@@ -16,8 +16,15 @@ public class Main{
 }
 
 
+
 // What is sc.nextInt()?
+
 // sc.nextInt() is a method of the Scanner class in Java that reads the next integer from the input.
-//  It waits for the user to enter an integer and then returns that integer as an int data type. 
+// It waits for the user to enter an integer and then returns that integer as an int data type. 
 // If the user enters something that is not an integer, it will throw an InputMismatchException.
 
+// What is sc.nextLine()?
+
+// sc.nextLine() is a method of the Scanner class in Java that reads the next line of input from the user.
+// It waits for the user to enter a line of text and then returns that line as a String data type.
+// If the user enters something and presses Enter, it will return that input as a String.
