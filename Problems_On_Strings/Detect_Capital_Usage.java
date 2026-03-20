186a5d5a63b5748740e87079a97f44c0cab7458d
OfficialDexter7 <dexyyy777@gmail.com>
2026-03-20 07:59:53 +0000

    commit #15

---
+++ Problems_On_Strings/Detect_Capital_Usage.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+
+        int upper = 0;
+
+        for(int i = 0; i < word.length(); i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                upper++;
+            }
+        }
+
+        if(upper == word.length() || upper == 0 || 
+           (upper == 1 && Character.isUpperCase(word.charAt(0))))
+            System.out.print("true");
+        else
+            System.out.print("false");
+    }
+}
