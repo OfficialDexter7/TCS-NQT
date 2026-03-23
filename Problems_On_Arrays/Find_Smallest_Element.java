cdc4a55a996e817f64eb4911a215d491edaa8270
OfficialDexter7 <dexyyy777@gmail.com>
2026-03-23 13:24:52 +0000

    commit #26

--- Problems_On_Arrays/Find_Smallest_Element.java
+++ Problems_On_Arrays/Find_Smallest_Element.java
@@ -9,24 +9,4 @@ public class Find_Smallest_Element{
         }
         return min;
     }
-
-
-        int min = arr[0];
-
-        for(int i = 1; i < n; i++){
-            if(arr[i] < min){
-                min = arr[i];
-            }
-        }
-       return min;
-    }
-    
-
-    int min = arr[0];
-
-    for(int i = 0; i < n; i++){
-        if(arr[i] < min){
-            min = arr[i];
-        }
-    }
-    return min;
+}
