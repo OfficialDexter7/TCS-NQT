04ddb9afe4cd10041c57c34109bca2217aa053aa
OfficialDexter7 <dexyyy777@gmail.com>
2026-03-23 07:42:07 +0000

    commit #22

---
+++ Basic_Questions/Student_Grade_System.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Student_Grade_System {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int marks = sc.nextInt();
+
+        if(marks >= 90){
+            System.out.println("Grade A");
+        } else if(marks >= 80){
+            System.out.println("Grade B");
+        } else if(marks >= 70){
+            System.out.println("Grade C");
+        } else if(marks >= 60){
+            System.out.println("Grade D");
+        } else if(marks >= 50){
+            System.out.println("Grade F");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
