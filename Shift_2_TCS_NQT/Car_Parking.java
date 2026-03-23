1cdc9fabb43932ae1752c62138e9b18f67543f60
OfficialDexter7 <dexyyy777@gmail.com>
2026-03-23 07:23:57 +0000

    commit #21

--- Shift_2_TCS_NQT/Car_Parking.java
+++ Shift_2_TCS_NQT/Car_Parking.java
@@ -1,27 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
 
-public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
+public class Car_Parking {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    if(!sc.hasNextInt()){
-        System.out.println("error");
-        return;
+        if (!sc.hasNextInt()) {
+            System.out.println("error");
+            return;
+        }
+
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("error");
+            return;
+        }
+
+        int cost = 0;
+
+        if (n <= 2) {
+            cost = n * 100;
+        } else if (n <= 5) {
+            cost = (2 * 100) + (n - 2) * 50;
+        } else {
+            cost = (2 * 100) + (3 * 50) + (n - 5) * 20;
+        }
+
+        System.out.println(cost);
     }
-
-    int n = sc.nextInt();
-
-    if(n < 0) {
-        System.out.println("error");
-        return;
-    }
-
-    int charge;
-
-    if(n <= 2){
-        charge = n * 30;
-    } else {
-        charge = 60 + (n - 2) * 50;
-    }
-
-    System.out.println(charge);
 }
